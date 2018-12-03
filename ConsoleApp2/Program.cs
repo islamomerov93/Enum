1071a132c8de8de5951302e8a3208c16c544b150
Omarov Islam Ilham  <Omar_zi02@ITSTEP.LOCAL>
2018-12-03 21:46:49 +0400

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EnumHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose profession : ");
+            Console.WriteLine("CEO       - 1 \nManager   - 2 \nDeveloper - 3");
+            var choice = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter worked time : ");
+            int wTime = Convert.ToInt32(Console.ReadLine());
+            if (Accauntant.AskForBonus((Worker)choice, wTime)) Console.WriteLine("Bonus Earned!");
+            else Console.WriteLine("Bonus not earned!");
+        }
+    }
+    enum Worker
+    {
+        CEO = 1,
+        Manager,
+        Developer
+    }
+    struct Accauntant
+    {
+        public static bool AskForBonus(Worker tmp, int hours)
+        {
+            switch (tmp)
+            {
+                case Worker.CEO:
+                    return hours > 190;
+                case Worker.Manager:
+                    return hours > 195;
+                case Worker.Developer:
+                    return hours > 200;
+                default:
+                    return false;
+            }
+        }
+    }
+}
